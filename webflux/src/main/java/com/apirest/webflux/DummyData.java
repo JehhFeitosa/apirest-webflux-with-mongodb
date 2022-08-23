6d88c6a3e20eb1d18521af0424edcb0f321f14d4
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2022-08-22 23:05:31 -0300

    disabling code that populates database

--- webflux/src/main/java/com/apirest/webflux/DummyData.java
+++ webflux/src/main/java/com/apirest/webflux/DummyData.java
@@ -1,29 +1,29 @@
-package com.apirest.webflux;
-
-import com.apirest.webflux.document.Playlist;
-import com.apirest.webflux.repository.PlaylistRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-import reactor.core.publisher.Flux;
-
-import java.util.UUID;
-
-@Component
-public class DummyData implements CommandLineRunner {
-
-    private final PlaylistRepository playlistRepository;
-
-    DummyData(PlaylistRepository playlistRepository){
-        this.playlistRepository = playlistRepository;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        playlistRepository.deleteAll()
-                .thenMany(Flux.just("API REST Spring Boot", "Deploy de uma aplicação java no IBM Cloud", "Java 11", "Github", "Spring Security", "Web Service RESTFULL", "Bean no Spring Framework")
-                .map(nome -> new Playlist(UUID.randomUUID().toString(), nome))
-                .flatMap(playlistRepository::save))
-                .subscribe(System.out::println);
-
-    }
-}
+//package com.apirest.webflux;
+//
+//import com.apirest.webflux.document.Playlist;
+//import com.apirest.webflux.repository.PlaylistRepository;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//import reactor.core.publisher.Flux;
+//
+//import java.util.UUID;
+//
+//@Component
+//public class DummyData implements CommandLineRunner {
+//
+//    private final PlaylistRepository playlistRepository;
+//
+//    DummyData(PlaylistRepository playlistRepository){
+//        this.playlistRepository = playlistRepository;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        playlistRepository.deleteAll()
+//                .thenMany(Flux.just("API REST Spring Boot", "Deploy de uma aplicação java no IBM Cloud", "Java 11", "Github", "Spring Security", "Web Service RESTFULL", "Bean no Spring Framework")
+//                .map(nome -> new Playlist(UUID.randomUUID().toString(), nome))
+//                .flatMap(playlistRepository::save))
+//                .subscribe(System.out::println);
+//
+//    }
+//}
