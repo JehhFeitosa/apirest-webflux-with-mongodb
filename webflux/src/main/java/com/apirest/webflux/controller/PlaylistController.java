7468b47bcb14f00a29a340a689bcf364e6e00e04
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2022-08-24 23:14:06 -0300

    Created services with events Stream

--- webflux/src/main/java/com/apirest/webflux/controller/PlaylistController.java
+++ webflux/src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -3,11 +3,19 @@ package com.apirest.webflux.controller;
 import com.apirest.webflux.document.Playlist;
 import com.apirest.webflux.services.PlaylistService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
-//@RestController
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@RestController
 public class PlaylistController {
 
     @Autowired
@@ -28,6 +36,15 @@ public class PlaylistController {
         return service.save(playlist);
     }
 
+    @GetMapping(value="/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlaylistByEvents(){
+
+        System.out.println("---Start get Playlists by WEBFLUX--- " + LocalDateTime.now());
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+        Flux<Playlist> events = service.findAll();
+
+        return Flux.zip(interval, events);
+    }
 
 
 }
